51aa3898f2ff84ec8fdf266efd860b4a8e004f70
rulkimi <azrulkimi00@gmail.com>
2025-09-21 23:14:06 +0800

    fix: show max of 5 friends

--- src/app/receipts/_components/receipt-card.tsx
+++ src/app/receipts/_components/receipt-card.tsx
@@ -10,13 +10,13 @@ import React from "react"
 type ReceiptCardProps = {
 	index: number;
 	length: number;
-  receipt: Receipt;
+	receipt: Receipt;
 }
 
 export default function ReceiptCard({
 	index,
 	length,
-  receipt
+	receipt
 }: ReceiptCardProps) {
 	const cardClass = cn(
 		"rounded-none border-t-0 border-b-0 gap-0",
@@ -25,42 +25,57 @@ export default function ReceiptCard({
 		length === 2 && index === 1 && "rounded-b-2xl border-b",
 		length > 2 && index === 0 && "rounded-t-2xl border-t",
 		length > 2 && index === length - 1 && "rounded-b-2xl border-b",
-    length > 2 && index !== length - 1 && "border-b"
+		length > 2 && index !== length - 1 && "border-b"
 	);
 
-  const persons = receipt.friends.length === 0
-    ? "Just you"
-    : `${receipt.friends.length} ${receipt.friends.length === 1 ? "person" : "persons"}`;
+	// const persons = receipt.friends.length === 0
+	// 	? "Just you"
+	// 	: `${receipt.friends.length} ${receipt.friends.length === 1 ? "person" : "persons"}`;
+
+	const maxAvatars = 5;
+	const showMore = receipt.friends.length > maxAvatars;
+	const visibleFriends = showMore ? receipt.friends.slice(0, maxAvatars) : receipt.friends;
+	const moreCount = receipt.friends.length - maxAvatars;
 
 	return (
 		<Link href={`/receipts/${receipt.id}`}>
-      <Card className={cn("hover:bg-muted/10 cursor-pointer", cardClass)}>
-        <CardHeader>
-          <CardTitle className="flex gap-1 justify-between">
-            <span>{receipt.restaurant_name}</span>
-            <span className="text-primary font-normal">
-              {formatCurrency(receipt.total_amount, receipt.currency)}
-            </span>
-          </CardTitle>
-        </CardHeader>
-        <CardContent className="">
-          <div className="flex justify-between">
-            <div className="flex items-center gap-1 text-muted-foreground">
-              <Calendar className="size-4" />
-              <span>{formatDateTime(receipt.created_at)}</span>
-            </div>
-            <div className="flex items-center gap-1 text-muted-foreground">
-              <Users className="size-4" />
-              <span>{persons}</span>
-              <AvatarGroup>
-                {receipt.friends.map(friend => (
-                  <FriendAvatar key={friend.id} friend={friend} />
-                ))}
-              </AvatarGroup>
-            </div>
-          </div>
-        </CardContent>
-      </Card>
-    </Link>
+			<Card className={cn("hover:bg-muted/10 cursor-pointer", cardClass)}>
+				<CardHeader>
+					<CardTitle className="flex gap-1 justify-between">
+						<span>{receipt.restaurant_name}</span>
+						<span className="text-primary font-normal">
+							{formatCurrency(receipt.total_amount, receipt.currency)}
+						</span>
+					</CardTitle>
+				</CardHeader>
+				<CardContent className="">
+					<div className="flex justify-between">
+						<div className="flex items-center gap-1 text-muted-foreground">
+							<Calendar className="size-4" />
+							<span>{formatDateTime(receipt.created_at)}</span>
+						</div>
+						<div className="flex items-center gap-1 text-muted-foreground">
+							<Users className="size-4" />
+							{/* <span>{persons}</span> */}
+							<AvatarGroup>
+								{visibleFriends.map(friend => (
+									<FriendAvatar key={friend.id} friend={friend} />
+								))}
+							</AvatarGroup>
+              {showMore && (
+                <span className="ml-1 text-xs font-medium text-muted-foreground">
+                  +{moreCount} more
+                </span>
+              )}
+              {receipt.friends.length === 0 && (
+                <span className="ml-1 text-xs font-medium text-muted-foreground">
+                  Just you
+                </span>
+              )}
+						</div>
+					</div>
+				</CardContent>
+			</Card>
+		</Link>
 	)
 }
