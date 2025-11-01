2b7895ca40c3373b6d3728a87531b3e9f56a46ac
rulkimi <azrulkimi00@gmail.com>
2025-11-01 21:17:55 +0800

    fix: add page layout

--- src/app/friends/page.tsx
+++ src/app/friends/page.tsx
@@ -1,5 +1,20 @@
+import {
+  PageContent,
+  PageHeader,
+  PageLayout,
+  PageTitle,
+} from "@/components/layout/page-layout";
 import FriendsList from "./_components/friends-list";
 
 export default function Friends() {
-  return <FriendsList />
+  return (
+    <PageLayout>
+      <PageHeader backHref="/home">
+        <PageTitle>Friends</PageTitle>
+      </PageHeader>
+      <PageContent>
+        <FriendsList />
+      </PageContent>
+    </PageLayout>
+  );
 }
